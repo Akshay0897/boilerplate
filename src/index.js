66e869e1de7292c2d8f62489e1c4969179f48215
Akshay0897 <akshayaghera05@gmail.com>
2020-08-27 15:01:46 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import "@babel/polyfill"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+)
